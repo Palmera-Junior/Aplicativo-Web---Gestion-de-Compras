01c4cf081a7c4edd4badc5799d8d2dc41e698524
Palmera-Junior <andres.corzo@palmerajunior.com>
2026-07-16 15:56:37 -0500

    Feat: Se implementó la funcionalida completa del <selec>Proveedor en el modal, para que llene los campos automaticamente

--- src/main/resources/static/dashboard.js
+++ src/main/resources/static/dashboard.js
@@ -36,4 +36,64 @@ document.addEventListener("DOMContentLoaded", () => {
         document.body.style.overflow = "auto";
     }
 
+    //Codigo de los campos del proveedor 
+
+    const selectProveedor = document.getElementById("select-proveedor");
+
+    const nit = document.getElementById("prov-nit");
+    const nombre = document.getElementById("prov-nombre");
+    const ciudad = document.getElementById("prov-ciudad");
+    const direccion = document.getElementById("prov-direccion");
+    const telefono = document.getElementById("prov-telefono");
+    const email = document.getElementById("prov-email");
+
+    const campos = [
+    nit,
+    nombre,
+    ciudad,
+    direccion,
+    telefono,
+    email
+];
+
+    selectProveedor.addEventListener("change", function () {
+
+        const opcion = this.options[this.selectedIndex];
+
+        // Opción "Otro"
+    if (this.value === "otro") {
+
+        campos.forEach(campo => {
+            campo.disabled = false;
+            campo.value = "";
+        });
+
+        return;
+    }
+
+    // Ningún proveedor seleccionado
+    if (this.value === "") {
+
+        campos.forEach(campo => {
+            campo.disabled = true;
+            campo.value = "";
+        });
+
+        return;
+    }
+
+        // Proveedor existente
+        nit.value = opcion.dataset.nit || "";
+        nombre.value = opcion.dataset.nombre || "";
+        ciudad.value = opcion.dataset.ciudad || "";
+        direccion.value = opcion.dataset.direccion || "";
+        telefono.value = opcion.dataset.telefono || "";
+        email.value = opcion.dataset.email || "";
+
+        campos.forEach(campo => {
+        campo.disabled = true;
+    });
+
+    });
 });
+
