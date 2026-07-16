1b2f26947053a638b2b6b503df79c60056d7758b
Andres Felipe Corzo Avellaneda <andres.corzo@palmerajunior.com>
2026-07-16 11:18:35 -0500

    Feat: se configuró el objeto ordenCompra, para mostrarlo en el Dashboard

--- src/main/java/com/palmera_junior/gestion_compras/controller/DashboardController.java
+++ src/main/java/com/palmera_junior/gestion_compras/controller/DashboardController.java
@@ -1,17 +1,39 @@
 package com.palmera_junior.gestion_compras.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.palmera_junior.gestion_compras.entity.OrdenCompra;
+import com.palmera_junior.gestion_compras.service.OrdenCompraService;
 
 
 
 @Controller
+
+
 public class DashboardController {
+
+    @Autowired
+    private OrdenCompraService ordenCompraService;
+
     @GetMapping("/dashboard")
-    public String dashboard() {
+    public String listarOrdenesCompra(
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "5") int size,
+        Model model) {
+
+       Page<OrdenCompra> ordenesCompra = ordenCompraService.ordenesDeCompraPaginadas(PageRequest.of(page, size));     
+
+
+        model.addAttribute("paginaActual", page);
+        model.addAttribute("ordenesCompra", ordenesCompra);
         return "dashboard";
     }
-    
     
 }
 
---
+++ src/main/java/com/palmera_junior/gestion_compras/service/OrdenCompraService.java
@@ -0,0 +1,22 @@
+package com.palmera_junior.gestion_compras.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import com.palmera_junior.gestion_compras.entity.OrdenCompra;
+import com.palmera_junior.gestion_compras.repository.OrdenCompraRepository;
+
+@Service
+public class OrdenCompraService {
+
+    @Autowired
+    private OrdenCompraRepository ordenCompraRepository;
+
+    public Page<OrdenCompra> ordenesDeCompraPaginadas(Pageable pageable) {
+        return ordenCompraRepository.findAll(pageable);
+    }
+
+
+}
