f6270a52ed9d8b3e6ca633c10f9bfdf5493ee450
Andres Felipe Corzo Avellaneda <andres.corzo@palmerajunior.com>
2026-07-16 15:34:59 -0500

    Style: se mejoró la vista del modal

--- src/main/java/com/palmera_junior/gestion_compras/repository/OrdenCompraRepository.java
+++ src/main/java/com/palmera_junior/gestion_compras/repository/OrdenCompraRepository.java
@@ -5,10 +5,11 @@ import java.util.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.palmera_junior.gestion_compras.entity.OrdenCompra;
 
-public interface OrdenCompraRepository extends JpaRepository<OrdenCompra, Integer>{
+public interface OrdenCompraRepository extends JpaRepository<OrdenCompra, Integer>, JpaSpecificationExecutor<OrdenCompra> {
 
     Page<OrdenCompra> findBySedeIdSedeOrderByFechaDesc(Integer idSede, Pageable pageable);
 
